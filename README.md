f4e93d59b1bc92d6d0d763b73d6bad0f5bcba46a
danielchim <kahungchim@gmail.com>
2022-05-07 10:46:05 +0800

    build: inital commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # fe-template
+
+A todo app written in typescript with react/materialUI
+## Install steps
+
+* Clone the repository
+* Run `yarn add`
+* Then run the `yarn run preinstall`
+* Run the `yarn run postinstall`
+* Next is the `yarn run tsc`
+* Finally you can start developing with `yarn run dev`
--- packages/fe/src/App.tsx
+++ packages/fe/src/App.tsx
@@ -1,22 +1,35 @@
-import { Button, Typography } from '@mui/material'
-import { ItemDB } from '@nlpdev/database'
+import { createTheme, ThemeProvider } from '@mui/material'
 import React from 'react'
 import create from 'zustand'
 
 const useStore = create(() => ({}))
 
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string
+    }
+  }
+  // 允许配置文件使用 `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string
+    }
+  }
+}
+
 export const App: React.FC = function App () {
   // ???
   useStore()
-  return (<>
-    <Typography variant='h1'>Hello world!</Typography>
-    <Button
-      onClick={() => {
-        // ???
-        ItemDB.create()
-      }}
-    >
-      Create
-    </Button>
-  </>)
+  const theme = createTheme({
+    status: {
+      danger: '#00FF00'
+    }
+  })
+
+  return (
+    <>
+      <ThemeProvider theme={theme}></ThemeProvider>
+    </>
+  )
 }
