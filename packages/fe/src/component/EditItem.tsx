95eab725321de52a8c0bc7a449da29086eda83ff
danielchim <kahungchim@gmail.com>
2022-05-16 15:59:52 +0800

    build: added update local storage feature

---
+++ packages/fe/src/component/EditItem.tsx
@@ -0,0 +1,47 @@
+import { Button, TextField } from '@mui/material'
+import React from 'react'
+
+interface IEditItemProps {
+  id: string
+  title: string
+  desc: string
+}
+
+const EditItem = ({ id, title, desc }: IEditItemProps) => {
+  const [newTitle, setNewTitle] = React.useState<string>(title)
+  const [newDesc, setNewDesc] = React.useState<string>(desc)
+  const updateLocalStorage = () => {
+    // update item into local storage, only update the item
+    const tmpObj: { id: string; title: string; desc: string } = { id, title: newTitle, desc: newDesc }
+    localStorage.setItem(id, JSON.stringify(tmpObj))
+  }
+  return (
+    <div>
+      <TextField
+        sx={{ width: 1 }}
+        id='outlined-basic'
+        label='Outlined'
+        variant='outlined'
+        defaultValue={title}
+        onChange={(e) => {
+          setNewTitle(e.target.value)
+        }}
+      />
+      <TextField
+        sx={{ width: 1 }}
+        id='outlined-basic'
+        label='Outlined'
+        variant='outlined'
+        defaultValue={desc}
+        multiline
+        rows={4}
+        onChange={(e) => {
+          setNewDesc(e.target.value)
+        }}
+      />
+      <Button onClick={updateLocalStorage}>Save</Button>
+    </div>
+  )
+}
+
+export default EditItem
--- packages/fe/src/pages/DetailsPage.tsx
+++ packages/fe/src/pages/DetailsPage.tsx
@@ -1,18 +1,24 @@
+import { Button } from '@mui/material'
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 
+import EditItem from './../component/EditItem'
+
 interface ISingleItem {
-  id: number
+  id: string
   title: string
   desc: string
 }
 
 const DetailsPage = () => {
   const singleItem: ISingleItem = useLocation().state as ISingleItem
+  const [showEdit, setShowEdit] = React.useState(false)
   return (
     <div>
       <h1>{singleItem.title}</h1>
       <p>{singleItem.desc}</p>
+      <Button onClick={() => setShowEdit(!showEdit)}> Edit me! </Button>
+      {showEdit ? <EditItem id={singleItem.id} title={singleItem.title} desc={singleItem.desc} /> : <></>}
     </div>
   )
 }
